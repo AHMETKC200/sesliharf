f6b1ff371913f289ccaf36ca7d3b070cb5ac2ce1
AHMETKC200 <89104029+AHMETKC200@users.noreply.github.com>
2022-04-21 06:40:14 +0300

    Add files via upload
    random.choice
    random.shuffle
    random.sample

---
+++ 690.py
@@ -0,0 +1,6 @@
+import random 
+liste=["yellow" ,"white", "mother", "father","brother" ,"sister" ]
+print(random.choice(liste))
+print(random.shuffle(liste))
+print(random.sample(liste,3))
+print(liste[1])
