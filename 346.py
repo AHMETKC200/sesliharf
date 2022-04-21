8979eb62e3c3c588ea3f728e4b0db9f2d6d5dbc1
AHMETKC200 <89104029+AHMETKC200@users.noreply.github.com>
2022-04-21 06:45:51 +0300

    Add files via upload
    linked files. Circumference and area of ​​the circle

---
+++ 346.py
@@ -0,0 +1,11 @@
+
+
+import nanana
+dm =nanana.çembercevresi(4)
+print(dm)
+import nanana
+km=nanana.cemberalanı(5)
+print(km)
+
+
+
---
+++ nanana.py
@@ -0,0 +1,15 @@
+import math
+number=int(input("sayi gir"))
+sonuc=math.sqrt(number)
+print(sonuc)
+fak=math.factorial(number)
+print(fak)
+bv=math.isqrt(number)
+
+
+def çembercevresi(r):
+    return 2*3.14*r
+def cemberalanı(r):
+    return 3.14*r*r
+
+
